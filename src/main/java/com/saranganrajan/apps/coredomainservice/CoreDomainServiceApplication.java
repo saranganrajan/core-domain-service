ae6afd5e7f5783d7c8639c3cfe803f08640e2004
saranganrajan <123263185+saranganrajan@users.noreply.github.com>
2023-03-04 13:32:39 +0800

    Add files via upload

---
+++ src/main/java/com/saranganrajan/apps/coredomainservice/CoreDomainServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saranganrajan.apps.coredomainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreDomainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreDomainServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saranganrajan/apps/coredomainservice/CoreDomainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saranganrajan.apps.coredomainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreDomainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
